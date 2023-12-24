cd011b2dc362c16a98a9c01f53024166977cdeda
cqrity <cqrity@list.ru>
2023-12-24 14:51:02 +0300

    done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './js/fold-items-list.js'
 import './js/modals.js'
 import './js/customer-form.js'
 import './js/cart-items.js'
-import { renderCartItems } from './js/cart-items.js'
+import { renderCartItems, renderMissingItems } from './js/cart-items.js'
 
 renderCartItems();
+renderMissingItems();
--- js/cart-items.js
+++ js/cart-items.js
@@ -1,73 +1,73 @@
 const cartItems = [
-    {
-        size: 56,
-        photoSrc: 'images/photo-01.jpg',
-        photoLargeSrc: 'images/photo-01@2x.jpg 2x',
-        photoDescription: 'Фото моделей в белых футболках',
-        priceWhole: '1051',
-        discount: 55,
-        priceDiscount: '522',
-        title: 'Футболка UZcotton мужская',
-        color: 'белый',
-        storage: 'Коледино WB',
-        seller: {
-            sellerName: 'Вайлдберриз',
-            ogrn: 1067746062449,
-            address: '142181, Московская область, д Коледино, тер. Индустриальный Парк Коледино, д. 6 стр. 1'
-        },
-        remainingAmount: 2,
+  {
+    size: 56,
+    photoSrc: 'images/photo-01.jpg',
+    photoLargeSrc: 'images/photo-01@2x.jpg 2x',
+    photoDescription: 'Фото моделей в белых футболках',
+    priceWhole: '1051',
+    discount: 55,
+    priceDiscount: '522',
+    title: 'Футболка UZcotton мужская',
+    color: 'белый',
+    storage: 'Коледино WB',
+    seller: {
+      sellerName: 'Вайлдберриз',
+      ogrn: 1067746062449,
+      address: '142181, Московская область, д Коледино, тер. Индустриальный Парк Коледино, д. 6 стр. 1'
     },
-    {
-        size: '',
-        photoSrc: 'images/photo-02.jpg',
-        photoLargeSrc: 'images/photo-02@2x.jpg 2x',
-        photoDescription: 'Фото телефона в прозрачном чехле',
-        priceWhole: '2 300 047',
-        discount: 55,
-        priceDiscount: '2 100 047',
-        title: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
-        color: 'прозрачный',
-        storage: 'Коледино WB',
-        seller: {
-            sellerName: 'Мегапрофстиль',
-            ogrn: 5167746237148,
-            address: '129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34'
-        },
-        remainingAmount: '',
+    remainingAmount: 2,
+  },
+  {
+    size: '',
+    photoSrc: 'images/photo-02.jpg',
+    photoLargeSrc: 'images/photo-02@2x.jpg 2x',
+    photoDescription: 'Фото телефона в прозрачном чехле',
+    priceWhole: '2 300 047',
+    discount: 55,
+    priceDiscount: '2 100 047',
+    title: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+    color: 'прозрачный',
+    storage: 'Коледино WB',
+    seller: {
+      sellerName: 'Мегапрофстиль',
+      ogrn: 5167746237148,
+      address: '129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34'
     },
-    {
-        size: '',
-        photoSrc: 'images/photo-03.jpg',
-        photoLargeSrc: 'images/photo-03@2x.jpg 2x',
-        photoDescription: 'Фото упаковки разноцветных карандашей',
-        priceWhole: '950',
-        discount: 55,
-        priceDiscount: '494',
-        title: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell',
-        color: '',
-        storage: 'Коледино WB',
-        seller: {
-            sellerName: 'Вайлдберриз',
-            ogrn: 1067746062449,
-            address: '142181, Московская область, д Коледино, тер. Индустриальный Парк Коледино, д. 6 стр. 1'
-        },
-        remainingAmount: 2,
+    remainingAmount: '',
+  },
+  {
+    size: '',
+    photoSrc: 'images/photo-03.jpg',
+    photoLargeSrc: 'images/photo-03@2x.jpg 2x',
+    photoDescription: 'Фото упаковки разноцветных карандашей',
+    priceWhole: '950',
+    discount: 55,
+    priceDiscount: '494',
+    title: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell',
+    color: '',
+    storage: 'Коледино WB',
+    seller: {
+      sellerName: 'Вайлдберриз',
+      ogrn: 1067746062449,
+      address: '142181, Московская область, д Коледино, тер. Индустриальный Парк Коледино, д. 6 стр. 1'
     },
+    remainingAmount: 2,
+  },
 ];
 
 const renderCartItems = () => {
-    cartItems.forEach((item) => {
-        const { size, photoSrc, photoLargeSrc, photoDescription, priceDiscount, priceWhole, discount, title, color, storage, seller: { sellerName, ogrn, address }, remainingAmount } = item;
-        document.querySelector(".cart-items-list").insertAdjacentHTML(
-            "beforeend",
-            `<li>
+  cartItems.forEach((item) => {
+    const { size, photoSrc, photoLargeSrc, photoDescription, priceDiscount, priceWhole, discount, title, color, storage, seller: { sellerName, ogrn, address }, remainingAmount } = item;
+    document.querySelector(".cart-items-list").insertAdjacentHTML(
+      "beforeend",
+      `<li>
                 <div class="item-card">
                   <div class="item-info">
                     <div class="item-photo-block">
                       <label class="item-photo-block-label">
                         <input type="checkbox" class="item-photo-checkbox visually-hidden" />
                       </label>
-                      <span class="item-size-beige">${size}</span>
+                      ${size ? `<span class="item-size-beige">${size}</span>` : ''}
                       <picture>
                         <img
                           class="item-photo"
@@ -95,11 +95,11 @@ const renderCartItems = () => {
                       </div>
                       <div class="item-description-block">
                         <p class="item-title">${title}</p>
-                        ${color || size ?
-                `
+                        ${color ?
+        `
                         <div class="item-color-size-block">
                             <p class="item-color">Цвет: ${color}</p>
-                            <p class="item-size">Размер: ${size}</p>
+                            ${size ? `<p class="item-size">Размер: ${size}</p>` : ''}
                         </div>
                         ` : ''}
                         <p class="item-storage">${storage}</p>
@@ -130,8 +130,49 @@ const renderCartItems = () => {
                 </div>
               </li>
       `
-        );
-    });
+    );
+  });
 };
 
-export { renderCartItems, cartItems };
+const renderMissingItems = () => {
+  cartItems.forEach((item) => {
+    const { size, photoSrc, photoLargeSrc, photoDescription, title, color } = item;
+    document.querySelector(".cart-items--missing").insertAdjacentHTML(
+      "beforeend",
+      `
+      <li class="item item-missed">
+      <div class="item-photo-block item-photo-block-missed">
+        <picture>
+          <img
+            class="item-photo"
+            src=${photoSrc}
+            srcset=${photoLargeSrc}
+            width="80"
+            alt=${photoDescription}
+          />
+        </picture>
+        ${size ? `<span class="item-size-beige">${size}</span>` : ''}
+      </div>
+      <div class="item-description-block item-description-block--missed">
+        <p class="item-title item-title--missing">${title}</p>
+        ${color ?
+        `
+        <div class="item-color-size-block">
+            <p class="item-color">Цвет: ${color}</p>
+            ${size ? `<p class="item-size">Размер: ${size}</p>` : ''}
+        </div>
+        ` : ''}
+      </div>
+      <div class="item-buttons-block item-buttons-block--missing">
+        <div class="item-misc-buttons item-misc-buttons--missing">
+          <button class="item-button-favorite item-button-favorite--missing"></button>
+          <button class="item-button-delete item-button-delete--missing"></button>
+        </div>
+      </div>
+    </li>
+    `
+    );
+  });
+};
+
+export { renderCartItems, renderMissingItems };
